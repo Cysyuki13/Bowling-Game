1cdbe063fbea4ee264a0933f0a2d2c2ecba3fd2e
Cysyuki <143718961+Cysyuki13@users.noreply.github.com>
2026-04-09 23:50:20 +0800

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// 2. Your Firebase configuration (compiled from the information you provided)
+const firebaseConfig = {
+    apiKey: "AIzaSyDP8tmDw_8E-oONCIIATHJppOvVCToMB5E",
+    authDomain: "bowling-game-cysyuki.firebaseapp.com",
+    // This is the most important connection address
+    databaseURL: "https://bowling-game-cysyuki-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "bowling-game-cysyuki",
+    storageBucket: "bowling-game-cysyuki.firebasestorage.app",
+    messagingSenderId: "786286743520",
+    appId: "1:786286743520:web:4eb5b6c50e84cae73aa585",
+    measurementId: "G-GG1MG6NZ9K"
+};
+
+// 3. Initialize Firebase and mount it to the global variable database
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.database();
+window.db.goOffline();
